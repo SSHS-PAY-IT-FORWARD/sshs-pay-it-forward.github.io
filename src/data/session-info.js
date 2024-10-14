d6c2176dce50549fbbefa7699a5f25b02d52e850
y123ob <y123ob@naver.com>
2024-10-15 07:06:50 +0900

    Update 2024 speaker-info

--- src/data/session-info.js
+++ src/data/session-info.js
@@ -19,12 +19,12 @@ export default [
     title: "Entertainment and Tech",
     description:
       "기술의 발전은 사용자의 엔터테인먼트 소비 환경을 어떻게 변화시켜왔고, 이에 따른 엔터테인먼트 업계 제작자, 창작자 그리고 소비자에게 어떤 영향을 주었는가. 앞으로 어떤 기회를 준비하고, 기술적 과제를 소화해야 하는가.",
-    moderator: { name: "류형규 CPO (2기, RLWRLD.AI)", id: 6 },
+    moderator: { name: "류형규 CPO (2기, RLWRLD.AI)", id: 1 },
     panels: [
-      { name: "김지원 사업부장 (8기, CJ E&M)", id: 7 },
-      { name: "박용희 대표 (10기, 블랙키 스튜디오)", id: 8 },
-      { name: "이승준 대표 (11기, AmazeVR)", id: 9 },
-      { name: "정민종 CTO (13기, JYP Entertainment)", id: 10 },
+      { name: "김지원 사업부장 (8기, CJ E&M)", id: 6 },
+      { name: "박용희 대표 (10기, 블랙키 스튜디오)", id: 7 },
+      { name: "이승준 대표 (11기, AmazeVR)", id: 8 },
+      { name: "정민종 CTO (13기, JYP Entertainment)", id: 9 },
     ],
   },
   {
@@ -33,12 +33,12 @@ export default [
     title: "Aerospace Technology",
     description:
       "지구의 온난화 및 이상 기후 등 작금의 상황과 과학 기술의 발전 속도를 보건데, 저 우주로 가는 날이 머지 않은 것 같다. 우주 시대가 과연 얼마나 다가왔으며, 어떤 연구 성과가 가시권에 왔는가. 우주 시대를 맞이하는 우리의 삶은 어떻게 변할 것이고, 우리에겐 재미있는 도전 과제가 기다리고 있는가.",
-    moderator: { name: "류석영 교수 (1기, KAIST)", id: 11 },
+    moderator: { name: "류석영 교수 (1기, KAIST)", id: 10 },
     panels: [
-      { name: "박창수 단장 (2기, 항공우주연구원)", id: 12 },
-      { name: "안재명 교수 (2기, KAIST)", id: 13 },
-      { name: "이복직 교수 (2기, 서울대)", id: 14 },
-      { name: "박훤 단장 (6기, 우주항공청)", id: 15 },
+      { name: "박창수 단장 (2기, 항공우주연구원)", id: 11 },
+      { name: "안재명 교수 (2기, KAIST)", id: 12 },
+      { name: "이복직 교수 (2기, 서울대)", id: 13 },
+      { name: "박훤 단장 (6기, 우주항공청)", id: 14 },
     ],
   },
   {
@@ -47,12 +47,12 @@ export default [
     title: "Big Tech. Corp.’s Challenge",
     description:
       "세상의 변화를 주도하는 큰 축 중 하나인 대기업들은 세상의 어떤 변화에 주목하고 대비, 도전하고 있는가. 기술 변화를 수용하고, 더 나은 세상을 만들기 위해 우리는 어떤 기회를 포착하고, 경험을 쌓는 것이 좋은가.",
-    moderator: { name: "류석영 교수 (1기, KAIST)", id: 16 },
+    moderator: { name: "류석영 교수 (1기, KAIST)", id: 10 },
     panels: [
-      { name: "정기현 부사장 (1기, LG전자)", id: 17 },
-      { name: "김경훈 사장 (4기, 구글코리아)", id: 18 },
-      { name: "안형기 전무 (4기, 현대자동차)", id: 19 },
-      { name: "이정원 부사장 (5기, 삼성전자)", id: 20 },
+      { name: "정기현 부사장 (1기, LG전자)", id: 15 },
+      { name: "김경훈 사장 (4기, 구글코리아)", id: 16 },
+      { name: "안형기 전무 (4기, 현대자동차)", id: 17 },
+      { name: "이정원 부사장 (5기, 삼성전자)", id: 18 },
     ],
   },
 ];
